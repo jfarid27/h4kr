b48f603ea6ad5b6846a0ea482619183b05ff93a2
J. Farid <jrab227@gmail.com>
2020-06-26 18:13:08 -0400

    Setting up prod/dev webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist/build/'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+module.exports = function(env, argv) {
+  return {
+    entry: './src/index.js',
+    mode: env.production ? 'production' : 'development',
+    devtool: env.production ? 'source-maps' : 'eval',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist/build/'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
         }
-      }
-    ]
-  }
+      ]
+    }
+  };
 };
