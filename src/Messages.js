c9604b399f5a535600cc447ed33f0fa392fe817e
J. Farid <jrab227@gmail.com>
2020-06-27 10:24:36 -0400

    Hacking Money

--- src/Messages.js
+++ src/Messages.js
@@ -1,41 +1,41 @@
 const Welcome = `
- __    __                      __                    
-/  |  /  |                    /  |                   
-$$ |  $$ |  ______    _______ $$ |   __              
-$$ |__$$ | /      \  /       |$$ |  /  |             
-$$    $$ | $$$$$$  |/$$$$$$$/ $$ |_/$$/              
-$$$$$$$$ | /    $$ |$$ |      $$   $$<               
-$$ |  $$ |/$$$$$$$ |$$ \_____ $$$$$$  \              
-$$ |  $$ |$$    $$ |$$       |$$ | $$  |             
-$$/   $$/  $$$$$$$/  $$$$$$$/ $$/   $$/              
-                                                     
- __       __                                         
-/  \     /  |                                        
-$$  \   /$$ |  ______   _______    ______   __    __ 
-$$$  \ /$$$ | /      \ /       \  /      \ /  |  /  |
-$$$$  /$$$$ |/$$$$$$  |$$$$$$$  |/$$$$$$  |$$ |  $$ |
-$$ $$ $$/$$ |$$ |  $$ |$$ |  $$ |$$    $$ |$$ |  $$ |
-$$ |$$$/ $$ |$$ \__$$ |$$ |  $$ |$$$$$$$$/ $$ \__$$ |
-$$ | $/  $$ |$$    $$/ $$ |  $$ |$$       |$$    $$ |
-$$/      $$/  $$$$$$/  $$/   $$/  $$$$$$$/  $$$$$$$ |
-                                           /  \__$$ |
-                                           $$    $$/ 
-                                            $$$$$$/  
+ /$$   /$$                     /$$       /$$                    
+| $$  | $$                    | $$      |__/                    
+| $$  | $$  /$$$$$$   /$$$$$$$| $$   /$$ /$$ /$$$$$$$   /$$$$$$ 
+| $$$$$$$$ |____  $$ /$$_____/| $$  /$$/| $$| $$__  $$ /$$__  $$
+| $$__  $$  /$$$$$$$| $$      | $$$$$$/ | $$| $$  \ $$| $$  \ $$
+| $$  | $$ /$$__  $$| $$      | $$_  $$ | $$| $$  | $$| $$  | $$
+| $$  | $$|  $$$$$$$|  $$$$$$$| $$ \  $$| $$| $$  | $$|  $$$$$$$
+|__/  |__/ \_______/ \_______/|__/  \__/|__/|__/  |__/ \____  $$
+                                                       /$$  \ $$
+                                                      |  $$$$$$/
+                                                       \______/ 
+ /$$      /$$                                                   
+| $$$    /$$$                                                   
+| $$$$  /$$$$  /$$$$$$  /$$$$$$$   /$$$$$$  /$$   /$$           
+| $$ $$/$$ $$ /$$__  $$| $$__  $$ /$$__  $$| $$  | $$           
+| $$  $$$| $$| $$  \ $$| $$  \ $$| $$$$$$$$| $$  | $$           
+| $$\  $ | $$| $$  | $$| $$  | $$| $$_____/| $$  | $$           
+| $$ \/  | $$|  $$$$$$/| $$  | $$|  $$$$$$$|  $$$$$$$           
+|__/     |__/ \______/ |__/  |__/ \_______/ \____  $$           
+                                            /$$  | $$           
+                                           |  $$$$$$/           
+                                            \______/            
 
-"You can hack money too!" - HackMoney.crypto 1.0.0-v7+ #1023
+"You can hack money too!" - HackingMoney.crypto 1.0.0-v7+ #1023
 
 The programs included with this system is free software;
 The exact distribution terms for each program are described in the
 individual files in the GPL-v3-or-later LICENSE.
 
-HackMoney.crypto comes with ABSOLUTELY NO WARRANTY, to the extent
+HackingMoney.crypto comes with ABSOLUTELY NO WARRANTY, to the extent
 permitted by applicable law.
 
 Questions? Contact: monteluna@protonmail.com
 
 =========================
 
-Welcome! This is HackMoney.crypto. A site for people to create digital
+Welcome! This is HackingMoney.crypto. A site for people to create digital
 social money on Ethereum. Please have Metamask or a WalletConnect enabled
 application. Have fun!!
 
