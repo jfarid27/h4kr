954fd7851d0e06d17d7f5eaad44fb545be2006d3
J. Farid <jrab227@gmail.com>
2020-07-13 20:15:54 -0400

    Adding initial layout.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React, { useState, useEffect, Component } from 'react';
+import React, { useState, useContext } from 'react';
 import ReactDOM from 'react-dom';
-import Terminal from 'terminal-in-react';
+import { Formik, Field, Form } from 'formik';
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import Web3 from 'web3';
 import {
   Welcome,
@@ -17,84 +23,147 @@ import {
   MintTokens
 } from './Actions';
 
-function Term() {
-  const [state, updateAppState] = useState({}); 
-  const web3 = state.web3;
-  return (<div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh"
-      }}
-    >
-      <Terminal
-        color='white'
-        backgroundColor='#17061d'
-        barColor='#17061d'
-        startState='maximised'
-        watchConsoleLogging
-        promptSymbol='$$'
-        style={{ fontWeight: "bold", fontSize: "1em" }}
-        commands={{
-          'connect': (args, print, runCommand) => {
-              Login(state, updateAppState, print);
-          },
-          'mint': {
-            method: (args, print, runCommand) => {
-              if (!state.loggedIn) return print("RequiresLoginException: Please login with `connect` first.");
-              if (!args._.length === 3) return print("ArgumentError: Requires 3 arguments. Type `mint --help` to see more.");
-              const name = args._[0];
-              const symbol = args._[1];
-              const amount = Number.parseInt(args._[2]);
-              if (Number.isNaN(amount)) return print("ArgmentError: Requires an integer amount of tokens. Type `mint --help` to see more.");
-              if (amount <= 0) return print("ArgmentError: Requires a positive integer amount of tokens. Type `mint --help` to see more.");
-              print("Creating Tokens:");
-              print(`   Name: ${name}`);
-              print(`   Amount: ${amount}`);
-              print('   Cost: 100 H4KR');
-              print(' ');
-              print(' ');
-              print('WARNING: Confirm gas prices in your transaction. This tool is not responsible for lost funds!');
-              print(' ');
-              print(' ');
-              print('Please approve your spending of 100 H4KR, then approve the token create transaction.');
-              MintTokens(name, symbol, amount, state, updateAppState, print);
-            },
-          },
-          'disconnect': (args, print, runCommand) => {
-              updateAppState(state => ({
-                ...state,
-                loggedIn: false,
-                contracts: null,
-                web3: null,
-                wallet: null
-              }));
-              print("Disconnected!")
-          },
-          'about': (args, print, runCommand) => {
-              print(About)
-          },
-          'contact': () => {
-            window.location.href = "mailto:monteluna@protonmail.com?subject=Contact from HackMoney.crypto&body=Hello%20World!"
-          },
-          
-        }}
-        descriptions={{
-          'about': AboutDesc,
-          'mint': MintDesc,
-          'connect': ConnectDesc,
-          'disconnect': DisconnectDesc,
-          'contact': 'contact the creator of this app'
-        }}
-        msg={Welcome}
-      />
-    </div>);
-}
+const createERC = "createERC";
 
+const submitNewTokenReady = (values) => {
+  if (values.action === createERC) {
+    if (!values.tokenname || values.tokenname === '') return;
+    if (!values.tokensymb || values.tokensymb === '') return;
+    if (!values.tokenamount || values.tokenamount <= 0) return;
+    return true;
+  }
+};
+
+const UserSelectors = ({ state, dispatch }) => (
+  <div>
+    <Formik
+     initialValues={{ action: "", tokenname: "", tokensymb: "", tokenamount: 0 }}
+     validate={values => {
+          const errors = {};
+          if (values.action === createERC) {
+            if (!values.tokenname || values.tokenname === '') { errors.tokenname = "Name error. Cannot be blank" };
+            if (!values.tokensymb || values.tokensymb === '') { errors.tokensymb = "Symbol error. Cannot be blank" };
+            if (!values.tokenamount || values.tokenamount <= 0) { errors.tokenamount = "Amount error. Cannot be less than zero." };
+          }
+         return errors;
+     }}
+     onSubmit={async (values, { setSubmitting }) => {
+       const { tokenname, tokensymbol, tokenamount } = values;
+       setSubmitting(true);
+       try {
+         await MintTokens(tokenname, tokensymbol, tokenamount, state, dispatch, () => ({}));
+       } catch (err) {
+         err
+       }
+       setSubmitting(false);
+     }}
+    >
+     {({
+       values,
+       errors,
+       touched,
+       handleChange,
+       handleBlur,
+       handleSubmit,
+       isSubmitting,
+       /* and other goodies */
+     }) => (
+       <Form onSubmit={handleSubmit}>
+         <div className="form-group">
+           <label htmlFor="action">I want to </label>
+           <Field as="select" className="my-select ml-2" name="action">
+             <option value="">{"select an action."}</option>
+             <option value={createERC}>{"mint a social token."}</option>
+           </Field>
+         </div>
+         { values.action === createERC ? <div className="form-group">
+           <label htmlFor="tokenname">The token name will be </label>
+           <input className="ml-2" name="tokenname" type="text" placeholder="Mytoken" 
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.tokenname}
+           />
+           { touched.tokenname && errors.tokenname && <p className="text-danger">{errors.tokenname}</p> }
+         </div> : "" }
+         { values.action === createERC ? <div className="form-group">
+           <label htmlFor="tokensymb">The token name will be </label>
+           <input className="ml-2" name="tokensymb" type="text" placeholder="MTK"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.tokensymb}
+           />
+           { touched.tokensymb && errors.tokensymb && <p className="text-danger">{errors.tokensymb}</p> }
+         </div> : "" }
+         { values.action === createERC ? <div className="form-group">
+           <label htmlFor="tokenamount">The amount that should be minted will be </label>
+           <input className="ml-2" name="tokenamount" type="number" placeholder="10000000"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.tokenamount}
+           />
+           { touched.tokenamount && errors.tokenamount && <p className="text-danger">{errors.tokenamount}</p> }
+         </div> : "" }
+          { values.action === createERC ?
+            <Button type="submit" variant="primary" block disabled={isSubmitting}>
+              Submit
+            </Button>
+            : ""
+          }
+       </Form>
+     )}
+    </Formik>
+  </div>
+);
+
+const printer = updateAppState => message => {
+    updateAppState(state => ( { ...state, message } )); 
+};
+
+const SelectorComponent = ({ state, dispatch }) => {
+  return (<div className="selector mt-5">
+    <Container>
+      <Row>
+        <Col xs={12}>
+          <UserSelectors state={state} dispatch={dispatch} />
+        </Col>
+      </Row>
+    </Container>
+  </div>);
+};
+
+
+const ConnectPromptComponent = ({ state, dispatch }) => {
+  const { loggedIn, wallet } = state;
+  const address = loggedIn && wallet && wallet.provider && wallet.provider.selectedAddress;
+  return (<div className="connection-prompt">
+    <Container>
+      <Row>
+        <Col xs={6}>
+          { (!loggedIn) ?
+            <Button variant="primary" onClick={() => Login(state, dispatch, printer(dispatch)) }>{"Connect"}</Button>
+            : 
+            <span>
+            <Button variant="primary" onClick={() => dispatch(st => ({ ...st, loggedIn: false })) }>{"Logout"}</Button>
+            </span> }
+        </Col>
+        <Col xs={{ span: 6 }}>
+          <h1 className="float-right">Hacking Money</h1>
+          { (loggedIn) ?
+              <p className="float-right text-info">{address}</p> : ""
+            }
+        </Col>
+      </Row>
+    </Container>
+  </div>);
+};
 
 function App() {
-  return <Term />
+  const [state, dispatch] = useState({});
+  const web3 = state.web3;
+  return <div className="mt-2">
+    <ConnectPromptComponent state={state} dispatch={dispatch} />
+    <SelectorComponent state={state} dispatch={dispatch} />
+  </div>;
 }
 
 const wrapper = document.getElementById("hackmoney-app");
