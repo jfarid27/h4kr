1eb9e8c40f3c246b584378554fd8e3b0b9d1f63c
Jalil F <jrab227@gmail.com>
2020-11-14 20:54:21 +0000

    Fixing public path.

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = function(env, argv) {
     output: {
       filename: 'main.js',
       path: path.resolve(__dirname, 'dist/build'),
+      publicPath: 'dist/build'
     },
     module: {
       rules: [
